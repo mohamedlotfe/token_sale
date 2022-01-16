d4561a3de22182db1ca3568f412d6541f44c2743
Muhammed Lotfe <mhmdlotfy9@gmail.com>
2022-01-16 11:15:47 +0200

    test

--- test/DappToken.js
+++ test/DappToken.js
@@ -5,6 +5,7 @@ contract('DappToken', function (accounts) {
         return DappToken.deployed().then(function (instance) {
             tokenInstance = instance;
             return tokenInstance.totalSupply();
+            
         }).then(function (totalSupply) {
             assert.equal(totalSupply.toNumber(), 1000000, 'sets the total supply to 1000,000 ');
         })
