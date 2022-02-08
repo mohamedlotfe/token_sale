15f0f7ff4d598ff5f835da91cd2fad0f72c3ce70
Muhammed Lotfe <mhmdlotfy9@gmail.com>
2022-02-08 09:31:05 +0200

    2. transfer tokens

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -1,12 +1,35 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract DappToken {
-    // Constructor
-    // set the total number of tokens
-    // read the total number of tokens
+    string public name = "DApp Token";
+    string public symbol = "DAPP";
+    string public standard = "DApp Token v1.0";
     uint256 public totalSupply;
 
-    constructor() {
-        totalSupply = 1000000;
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+
+    mapping(address => uint256) public balanceOf;
+
+    constructor(uint256 _initialSupply) {
+        balanceOf[msg.sender] = _initialSupply;
+        totalSupply = _initialSupply;
+    }
+
+    // Transfer
+    function transfer(address _to, uint256 _value)
+        public
+        returns (bool success)
+    {
+        require(balanceOf[msg.sender] >= _value);
+
+        // Transfer the balance
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+
+        // Transfer event
+        emit Transfer(msg.sender, _to, _value);
+
+        // Return a boolean
+        return true;
     }
 }
